c11fc19971716a1af80a28d127edcaa7cd216369
poonpawa <poonampawar0508@gmail.com>
2020-07-28 14:07:28 +0100

    Added tab navigation along with react navigation upgrade from version 4 to 5

---
+++ OnDemandDelivery/navigation/navigator.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/home';
+import Login from '../screens/login';
+import Register from '../screens/register';
+import Loading from '../screens/loading';
+import orderListing from '../screens/orderListing';
+import { Icon } from 'react-native-elements';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const bottomNavigation = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Order" component={OrderNavigation}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Icon type='octicon' name='package' />
+                    )
+                }} />
+            <Tab.Screen name="OrderHistory" component={orderListing}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Icon name='history' />
+                    )
+                }} />
+            <Tab.Screen name="Account" component={Home}
+                options={{
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Icon name='account-box' />
+                    )
+                }} />
+        </Tab.Navigator>
+    )
+}
+
+const OrderNavigation = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="availabilityScreen" component={Home} />
+            <Stack.Screen name="orderListing" component={orderListing} />
+            <Stack.Screen name="orderDetails" component={orderListing} />
+        </Stack.Navigator>
+    )
+}
+
+const AppNavigation = () => {
+    return (
+        <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={bottomNavigation} />
+        </Stack.Navigator>
+    )
+}
+
+const AuthNavigation = () => {
+    return (
+        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+    )
+
+}
+
+const navigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Loading" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Loading" component={Loading} />
+                <Stack.Screen name="App" component={AppNavigation} />
+                <Stack.Screen name="Auth" component={AuthNavigation} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default navigator;
--- OnDemandDelivery/screens/orderListing.js
+++ OnDemandDelivery/screens/orderListing.js
@@ -3,28 +3,44 @@ import { StyleSheet, View } from 'react-native'
 import { Button, Card, Text, Icon } from 'react-native-elements';
 
 export default function orderListing({ navigation }) {
-    let orderDetails = navigation.state.params.payload.data;
-    return (
-        <View>
-            <Card title='Order Details'>
-                <Text style={{ marginBottom: 10 }}>
-                    Order Id : {orderDetails.orderNumber}
-                </Text>
-                <Text style={{ marginBottom: 10 }}>
-                    Time : {orderDetails.time}
-                </Text>
-                <Text style={{ marginBottom: 10 }}>Person : {orderDetails.number}</Text>
-                <Text style={{ marginBottom: 10 }}>Address : {orderDetails.address}</Text>
-                <Text style={{ marginBottom: 10 }}>Store : {orderDetails.store}</Text>
-                <Button
-                    buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-                    title='Accept' />
-                <Button
-                    buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-                    title='Reject' />
-            </Card>
-        </View>
-    )
+    if (navigation.state) {
+        let orderDetails = navigation.state.params.payload.data;
+        return (
+            <View>
+                <Card title='Order Details'>
+                    <Text style={{ marginBottom: 10 }}>
+                        Order Id : {orderDetails.orderNumber}
+                    </Text>
+                    <Text style={{ marginBottom: 10 }}>
+                        Time : {orderDetails.time}
+                    </Text>
+                    <Text style={{ marginBottom: 10 }}>Person : {orderDetails.number}</Text>
+                    <Text style={{ marginBottom: 10 }}>Address : {orderDetails.address}</Text>
+                    <Text style={{ marginBottom: 10 }}>Store : {orderDetails.store}</Text>
+                    <Button
+                        buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+                        title='Accept' />
+                    <Button
+                        buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+                        title='Reject' />
+                </Card>
+            </View>
+        )
+    } else {
+        return (
+            <View style={styles.container}>
+                <Text h4>No New Order</Text>
+                <Text>You will get a notification when a new order is received</Text>
+            </View>
+        )
+    }
+
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
